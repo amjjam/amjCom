e19ccca22773de6251ee17ac009b8d841d9cf920
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2023-07-24 10:26:44 -0600

    Modified the split functions to accommodate more than one ":" in the communicator description.

--- src/amjCom.C
+++ src/amjCom.C
@@ -1,9 +1,9 @@
 #include "../include/amjCom.H"
 
-std::string split1(std::string &s){
+std::string split1(std::string s){
   return s.substr(0,s.find_first_of(':'));
 }
 
-std::string split2(std::string &s){
+std::string split2(std::string s){
   return s.substr(s.find_first_of(':')+1);
 }
