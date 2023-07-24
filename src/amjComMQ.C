5a505a223bfc55617b1c32be3e61f54dd99ecb57
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2023-07-24 10:25:46 -0600

    Added code to implement communication via message queues

---
+++ src/amjComMQ.C
@@ -0,0 +1,105 @@
+#include "../include/amjComMQ.H"
+
+#include <iostream>
+
+amjComEndpointMQ::amjComEndpointMQ(std::string send, std::string receive){
+  qsend=qreceive=-1;
+  initialize(send,SEND);
+  initialize(receive,RECEIVE);
+}
+
+amjComEndpointMQ::~amjComEndpointMQ(){
+  if(qsend>=0)
+    mq_close(qsend);
+  if(qreceive>=0)
+    mq_close(qreceive);
+}
+
+void amjComEndpointMQ::init(std::string description, int direction){
+  std::string name=split1(description);
+  int n=std::stoi(split1(split2(description)));
+  int size=std::stoi(split2(split2(description)));
+
+  // Open the queue in non-blocking mode
+  int flags=O_CREAT|O_NONBLOCK|(direction==SEND?O_WRONLY:O_RDONLY);
+  //attr.mq_flags=O_NONBLOCK;
+  struct mq_attr attr;
+  attr.mq_maxmsg=n;
+  attr.mq_msgsize=size;
+  // There are some permission issues which I don't fully 
+  // understand, but the code related to umask, which surrounds
+  // the call, is supposed to fix this. I found the solution here:
+  // http://stackoverflow.com/questions/22780277/
+  //                      mq-open-eacces-permission-denied
+  mode_t omask;
+  omask=umask(0);
+  mqd_t q;
+  if((q=mq_open(name.c_str(),flags,(S_IRWXU|S_IRWXG|S_IRWXO),&attr))<0){
+    char message[100];
+    sprintf(message,"Could not open message queue %s",name.c_str());
+    perror(message);
+    exit(1);
+  }
+  umask(omask);
+
+  // Verify queue creation with correct attributes
+  mq_getattr(q,&attr);
+  if(attr.mq_maxmsg!=n||attr.mq_msgsize!=size){
+    std::cout << "Error: A message queue with the name " << name
+	      << " already exists," << std::endl;
+    std::cout << "but with different attributes:" << std::endl;
+    std::cout << "Existing queue specs: n=" << attr.mq_maxmsg
+	      << ", size=" << attr.mq_msgsize << std::endl;
+    std::cout << "Desired queue spects: n=" << n << ", size="
+	      << size << std::endl;
+    std::cout << "The solution is to delete the queue and start again."
+	      << std::endl;
+    std::cout << "Use ipcs to get a list of message queues." << std::endl;
+    std::cout << "Use ipcrm to remove the message queue." << std::endl;
+    exit(1);
+  }
+
+  // If the queue is readable then flush it by reading messages until
+  // none are left. 
+  std::vector<char> dummy(size);
+  while(mq_receive(q,dummy.data(),size,nullptr)>=0);
+
+  // Set the queue to blocking if receiving from it and non-blocking
+  // if sending to it.
+  attr.mq_flags=(direction==SEND?O_NONBLOCK:0);
+  mq_setattr(q,&attr,nullptr);
+
+  // Set qsend or qreceive equal to this.
+  if(direction==SEND)
+    qsend=q;
+  else
+    qreceive=q;
+}
+
+int amjComEndpointMQ::send(amjPacket &p){
+  if(p.size()>_sendsize){
+    std::cout << "error: packet too big: " << p.size()
+	      << ". Queue size is " << _sendsize << std::endl;
+    exit(1);
+  }
+  int r=mq_send(qsend,p._data(),p.size(),1);
+  if(r>=0)
+    return r;
+  if(r==-1&&errno==EAGAIN)
+    return 0;
+  perror("amjComEndpointMQ::send error");
+  exit(1);
+  return 0;
+}
+
+int amjComEndpointMQ::receive(amjPacket &p){
+  p.resize(_receivesize);
+  int r=mq_receive(qreceive,p._data(),_receivesize,nullptr);
+  if(r>=0){
+    p.resize(r);
+    return r;
+  }
+  perror("amjComEndpointMQ::receive error");
+  exit(1);
+  return 0;
+}
---
+++ test/src/mqtest.C
@@ -0,0 +1,73 @@
+/* For testing message queue. Make it similar to UDP test */
+
+/*============================================================================
+  mqtest -r|-s -qsend <string> -qreceive <string>
+  -r will receive in an infinite loop and print messages
+  -s will send a message ever 1 s: "Hello from "+send string
+  -qsend /<name>:n:size
+  -qreceive /<name>:n:size
+     for both <name> is the POSIX message queue name, n is the number of
+     buffers in the queue, and size is the size of each buffer.
+  ============================================================================*/
+
+#include "../../include/amjComMQ.H"
+
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+
+void parse_args(int argc, char *argv[]);
+
+bool rtsf;
+std::string qsend;
+std::string qreceive;
+
+int main(int argc, char *argv[]){
+  parse_args(argc,char *argv[]);
+
+  amjComEndpointMQ(qsend,qreceive);
+  amjPacket p;
+
+  if(rtsf){ // I am a receiver
+    for(;;){
+      e.receive(p);
+      p.reset();
+      std::string message;
+      int number;
+      p >> number >> message;
+      std::cout << number << " " << message << std::endl;
+    }
+  }
+  else{ // I am a sender
+    for(int i=0;;i++){
+      std::string m="Hello from "+qsend;
+      p.clear();
+      p << i << m;
+      e.send(p);
+      usleep(100000);
+    }
+  }
+
+  return 0;      
+}
+
+void parse_args(int argc, char *argv[]){
+  for(int i=1;i<argc;i++){
+    if(strcmp(argv[i],"-r")==0)
+      rtsf=true;
+    else if(strcmp(argv[i],"-s")==0)
+      rtsf=false;
+    else if(strcmp(argv[i],"-qsend")==0){
+      i++;
+      qsend=argv[i];
+    }
+    else if(strcmp(argv[i],"-qreceive")==0){
+      i++;
+      qreceive=argv[i];
+    }
+    else{
+      std::cout << "error: unknown parameter: " << argv[i] << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+}
